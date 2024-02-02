923d007789a165e1fcffad1d1b51a15676748084
sevonj <100710152+sevonj@users.noreply.github.com>
2024-02-02 16:12:07 +0200

    follow system theme

---
+++ lib/themes/dark.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple, brightness: Brightness.dark),
+  useMaterial3: true,
+);
---
+++ lib/themes/light.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple, brightness: Brightness.light),
+  useMaterial3: true,
+);
