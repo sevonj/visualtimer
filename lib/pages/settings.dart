754552524ecc09000b72d377daa3f031f8e62314
sevonj <100710152+sevonj@users.noreply.github.com>
2024-02-02 21:02:43 +0200

    rename-appearance-settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:visualtimer/pages/settings/set_theme.dart';
+import 'package:visualtimer/pages/settings/appearance_settings.dart';
 
 _launchUrl() async {
   var url = Uri.parse("https://github.com/sevonj/visualtimer");
